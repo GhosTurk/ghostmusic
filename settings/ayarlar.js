79fd150068397943d8e44d2ad01917a147aee40e
Kadir <99703535+GhosTurk@users.noreply.github.com>
2023-04-16 19:18:03 +0300

    Add files via upload

---
+++ settings/ayarlar.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN || "TOKEN GİR",  // bot token gir :)
+    PREFIX: process.env.PREFIX || ".", // bot prefix
+    SAHİP: process.env.SAHİP || "461284057444122665", //sahip id :()
+}
